f361488079dae27f5c2aba92671bdf82ebfeed86
Petter Solberg <pettersolberg88@gmail.com>
2012-02-15 13:40:19 +0100

    Lol

--- EIT prosjektet/src/no/ntnu/eit/HelloGitHub.java
+++ EIT prosjektet/src/no/ntnu/eit/HelloGitHub.java
@@ -7,6 +7,8 @@ public class HelloGitHub extends TestbedTest {
 	public static void main(String[] args) {
 		System.out.println("Hello Github!");
 		System.out.println("Petter er den beste av de beste på gruppen og alle og magne er veldig enig!!!");
+		
+		System.out.println("DETTE BLE DEXEMT");
 	}
 
 	@Override
