b148bc1f2cc4f4d37ab185ffbeb4fe7ee261181d
magne <magne@ubuntu>
2012-02-15 13:41:47 +0100

    la til tekst

--- EIT prosjektet/src/no/ntnu/eit/HelloGitHub.java
+++ EIT prosjektet/src/no/ntnu/eit/HelloGitHub.java
@@ -5,8 +5,9 @@ import org.jbox2d.testbed.framework.TestbedTest;
 public class HelloGitHub extends TestbedTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Github!");
-		System.out.println("Petter er den beste av de beste på gruppen og alle og magne er veldig enig!!!");
+		
+		System.out.println("Hello Petter, Magne is back!");
+		System.out.println("Petter er den beste av de beste pï¿½ gruppen og alle og magne er veldig enig!!!");
 	}
 
 	@Override
